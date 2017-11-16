333e9a0a31d9cc2dcd43132b240f9a246310a38a
juancarlospaco <juancarlospaco@gmail.com>
2017-11-16 05:00:13 -0300

    Add uJSON support, Fix #1

--- apistar_msgpack.py
+++ apistar_msgpack.py
@@ -5,11 +5,16 @@
 """MessagePack Renderer/Parser for Apistar, see https://msgpack.org."""
 
 
-import json
 import typing
 
 from apistar import exceptions, http
 from apistar.renderers import Renderer
+
+try:
+    from ujson import loads  # https://github.com/esnme/ultrajson#ultrajson
+    JSONDecodeError = Exception
+except ImportError:
+    from json import loads, JSONDecodeError
 
 try:
     import msgpack   # https://github.com/msgpack/msgpack-python  (Official)
@@ -49,11 +54,19 @@ class MessagePackParser():
             raise exceptions.BadRequest(detail=f'Empty MessagePack: {body}.')
         try:
             if has_msgpack:
-                return json.loads(msgpack.unpackb(body))
+                data_from_msgpack = loads(msgpack.unpackb(body))
             else:
-                return json.loads(umsgpack.unpackb(body))
-        except json.JSONDecodeError as error:
+                data_from_msgpack = loads(umsgpack.unpackb(body))
+        except UnicodeEncodeError as error:
+            raise exceptions.BadRequest(
+                detail=f'Invalid Unicode UTF-8 on MessagePack error: {error}.')
+        except ValueError as error:
+            raise exceptions.BadRequest(
+                detail=f'Invalid Keys or Values on MessagePack error: {error}')
+        except JSONDecodeError as error:
             raise exceptions.BadRequest(detail=f'Invalid MessagePack: {error}')
         except Exception as error:
             raise exceptions.BadRequest(
-                detail=f'MessagePackParser Unknown Exception: {self} {error}.')
+                detail=f'{self} Unknown Exception: {error}, parsing {body}.')
+        else:
+            return data_from_msgpack
